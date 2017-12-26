ddf1796773964139679e56301f24e117e9117016
ton <ttonmaii@gmail.com>
2017-12-26 22:00:56 +0700

    first update

--- app/src/main/java/com/example/ton/furnituretabapplication/MainActivity.java
+++ app/src/main/java/com/example/ton/furnituretabapplication/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.ton.furnituretabapplication;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBth;
+    EditText username,password;
+    String usernameTxt,passwordTxt;
+    //Database
+    SQLiteDatabase mDb;
+    //DatabaseHelper mHelper;
+    //ArrayList<TBUserLoginModel> loginUser = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
