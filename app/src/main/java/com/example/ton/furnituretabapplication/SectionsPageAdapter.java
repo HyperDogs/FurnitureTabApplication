de31db921e6c13f631fa476d11409ce9e542d6d4
ton <ttonmaii@gmail.com>
2018-01-28 18:58:46 +0700

    Fragment

--- app/src/main/java/com/example/ton/furnituretabapplication/SectionsPageAdapter.java
+++ app/src/main/java/com/example/ton/furnituretabapplication/SectionsPageAdapter.java
@@ -3,6 +3,8 @@ package com.example.ton.furnituretabapplication;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,5 +40,15 @@ public class SectionsPageAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return mFragmentList.size();
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        if (position >= getCount()) {
+            FragmentManager manager = ((Fragment) object).getFragmentManager();
+            FragmentTransaction trans = manager.beginTransaction();
+            trans.remove((Fragment) object);
+            trans.commit();
+        }
     }
 }
