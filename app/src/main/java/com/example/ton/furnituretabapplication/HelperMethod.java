97c5904d6bb5c21994366ebd4f882efc4e054b31
ton <ttonmaii@gmail.com>
2018-02-08 23:14:06 +0700

    Fragment

--- app/src/main/java/com/example/ton/furnituretabapplication/HelperMethod.java
+++ app/src/main/java/com/example/ton/furnituretabapplication/HelperMethod.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.util.Log;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -37,7 +37,8 @@ public class HelperMethod extends Activity {
                         if (position == 0){
                             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                             filePagei = CreateFile.createUnique();
-                            uri = Uri.fromFile(filePagei);
+                            //uri = Uri.fromFile(filePagei);
+                            uri = FileProvider.getUriForFile(mContext,BuildConfig.APPLICATION_ID + ".provider",filePagei);
                             intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
                             ((Activity)mContext).startActivityForResult(intent, imageViewInt);
                         }else if (position == 1){
